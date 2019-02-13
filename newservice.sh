9f07fb69f6cb0ae223f7b7e720630f41a5978132
cwahlquist <cwahlquist@circadence.com>
2019-02-13 18:02:42 +0000

    Copyover script

---
+++ newservice.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+printf "\nWhat is the service name?\n"
+read serviceName
+
+printf "\nWhat is the repo path?\n"
+read repoPath
+
+printf "\n"
+
+cp -r ./* $repoPath
+
+mv $repoPath/charts/go-template $repoPath/charts/$serviceName
+find $repoPath -type f -exec \
+    sed -i "s/go-template/$serviceName/g" {} +
+find $repoPath -type f -exec \
+    sed -i "s/gotemplate/$serviceName/g" {} +
+serviceName="$(tr '[:lower:]' '[:upper:]' <<< ${serviceName:0:1})${serviceName:1}"
+find $repoPath -type f -exec \
+    sed -i "s/Gotemplate/$serviceName/g" {} +
